0e42db10326d6c8d41d7f756e2eabdce7f98f10d
Vikram Kumar <145278357+vikram-2101@users.noreply.github.com>
2023-11-12 21:17:23 +0530

    Add files via upload

---
+++ Arrays/RangeQuery.java
@@ -0,0 +1,30 @@
+import java.util.*;
+public class RangeQuery {
+    public static void main(String[] args) {
+     Scanner sc = new Scanner(System.in);
+     System.out.println("Enter the array size ");
+     int n = sc.nextInt();
+     int arr[] = new int[n];
+     for(int i=0; i<n; i++) {
+        arr[i] = sc.nextInt();
+     }
+     int prefSum[] = makePrefixSumArray(arr);
+     int q = sc.nextInt();
+     while(q>0) {
+     System.out.println("Enter the range ");
+     int l = sc.nextInt();
+     int r = sc.nextInt();
+    
+     int ans = prefSum[r]-prefSum[l-1];
+     System.out.println(ans);
+     q--;
+   }
+    }
+   static int[] makePrefixSumArray(int[] arr) {
+   for(int i=1; i<arr.length; i++) {
+    arr[i]+=arr[i-1];
+}
+return arr;
+   }
+
+}
