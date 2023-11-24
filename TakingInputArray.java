d3730fa4378850277adcda3c1b7f8230185e3e39
Vikram Kumar <145278357+vikram-2101@users.noreply.github.com>
2023-11-24 21:46:43 +0530

    Update and rename arrays2.java to TakingInputArray.java

--- TakingInputArray.java
+++ TakingInputArray.java
@@ -1,15 +1,15 @@
 import java.util.*;
 public class arrays2 {
     public static void main(String  args[]) {
-      //  int size = sc.nextInt();
-      //  String name[] = new String[size];
+       int size = sc.nextInt();
+       String name[] = new String[size];
        
-      //  for(int i=0; i<size; i++) {
-      //   name[i] = sc.next();
-      //  }
-      //  for(int i=0; i<name.length; i++) {
-      //    System.out.println("name "+(i+1)+ "is :"+ name[i]);
-      //  }
+       for(int i=0; i<size; i++) {
+        name[i] = sc.next();
+       }
+       for(int i=0; i<name.length; i++) {
+         System.out.println("name "+(i+1)+ "is :"+ name[i]);
+       }
 
       Scanner in = new Scanner(System.in);
       int[] arr =  {23,45,32,43,56,7};
