aad8184aaa66fd5fc215b611a9d5df17b2d9fd9d
Vikram Kumar <145278357+vikram-2101@users.noreply.github.com>
2023-11-26 19:09:14 +0530

    Update ArraysIntroduction.java

--- ArraysIntroduction.java
+++ ArraysIntroduction.java
@@ -4,7 +4,7 @@ public class arrays {
              int[] marks = new int[3];
     marks[0] = 97;
       marks[1] = 98;
-    // //  marks[2] = 95;
+    //  marks[2] = 95;
     // int marks[]= {97,98,95};
     // for(int i=0; i<3; i++) {
     //     System.out.println(marks[i]);
