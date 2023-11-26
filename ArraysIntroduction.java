f26983dc0a9a4beefae14ac0df6cbd4976ffc494
Vikram Kumar <145278357+vikram-2101@users.noreply.github.com>
2023-11-26 19:07:35 +0530

    Update ArraysIntroduction.java

--- ArraysIntroduction.java
+++ ArraysIntroduction.java
@@ -1,7 +1,7 @@
 import java.util.*;
 public class arrays {
     public static void main(String[] args) {
-    //         //int[] marks = new int[3];
+    //         int[] marks = new int[3];
     // //  marks[0] = 97;
     // //  marks[1] = 98;
     // //  marks[2] = 95;
