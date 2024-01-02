7be19504d9f337eefa55b7d0130c9f9280d45aee
Vikram Kumar <145278357+vikram-2101@users.noreply.github.com>
2024-01-02 20:49:44 +0530

    Update EqualSumPartition.java

--- EqualSumPartition.java
+++ EqualSumPartition.java
@@ -1,4 +1,4 @@
-hgffsgfsdimport java.util.*;
+hhgffsgfsdimport java.util.*;
 public class EqualSumPartition {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
