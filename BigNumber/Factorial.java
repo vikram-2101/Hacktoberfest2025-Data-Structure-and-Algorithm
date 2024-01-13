326fc6c55b27dc23c8f7e98c71f4a3a9b83296ca
Vikram Kumar <145278357+vikram-2101@users.noreply.github.com>
2024-01-13 13:59:30 +0530

    Add files via upload

---
+++ BigNumber/Factorial.java
@@ -0,0 +1,17 @@
+package BigNumber;
+import java.math.BigInteger;
+ class Factorial {
+    
+    static BigInteger fact(int num) {
+        BigInteger ans = new BigInteger("1");
+
+        for (int i = 2; i <=num; i++) {
+            ans = ans.multiply(BigInteger.valueOf(i));
+    }
+    return ans;
+}
+
+public static void main(String[] args) {
+    System.out.println(Factorial.fact(34));
+}
+ }
---
+++ BigNumber/Main.java
@@ -0,0 +1,28 @@
+package BigNumber;
+
+import java.math.BigInteger;
+
+ class Main {
+    public static void main(String[] args) {
+        int a = 30;
+        int b = 45;
+
+        BigInteger A = BigInteger.valueOf(33);
+        BigInteger B = BigInteger.valueOf(34);
+        BigInteger C = new BigInteger("3728397520");
+        BigInteger x = new BigInteger("89");
+        // constants
+        BigInteger D = BigInteger.ONE;
+
+        BigInteger s = C.add(x);
+        int r = C.intValue();
+        
+        BigInteger x2 = C.multiply(x);
+
+        // System.out.println(x2);
+
+        System.out.println(Factorial.fact(345));
+
+    }
+
+}
