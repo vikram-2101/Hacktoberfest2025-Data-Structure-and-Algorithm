14daa2f0045c8ea5d2a8689bdf5f5386355967bc
Vikram Kumar <145278357+vikram-2101@users.noreply.github.com>
2024-01-15 22:08:04 +0530

    code related to hashmaps

---
+++ HashMap/MapUsingHash.java
@@ -0,0 +1,41 @@
+package HashMap;
+
+import java.util.*;
+
+public class MapUsingHash {
+    
+    private Entity[] entities;
+
+    public MapUsingHash() {
+        entities = new Entity[100];
+    }
+    public void put(String key, String value) {
+        int hash = Math.abs(key.hashCode() % entities.length);
+        entities[hash] = new Entity(key,value);
+    }
+
+    public String get(String key) {
+        int hash = Math.abs(key.hashCode() % entities.length);
+        if(entities[hash] != null && entities[hash].key.equals(key)) {
+            return entities[hash].value;
+        }
+        return null;
+    }
+
+    public void remove(String key) {
+        int hash = Math.abs(key.hashCode() % entities.length);
+     if(entities[hash] != null && entities[hash].key.equals(key)) {
+  entities[hash]=null;
+    }
+}
+
+private class Entity {
+    String key;
+    String value;
+
+    public Entity(String key, String value) {
+        this.key = key;
+        this.value = value;
+    }
+}
+}
