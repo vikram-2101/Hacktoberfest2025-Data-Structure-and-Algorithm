08d13774cb2def162f3ba3d25ffbd9fbf55911e6
Vikram Kumar <145278357+vikram-2101@users.noreply.github.com>
2024-01-29 19:23:39 +0530

    Code related to stack

---
+++ Stack/StackMain.java
@@ -0,0 +1,17 @@
+package Stack;
+
+public class StackMain {
+
+    public static void main(String[] args) throws StackException {
+        CustomStack stack = new DynamicStack(5);
+
+        stack.push(3);
+        stack.push(34);
+        stack.push(75);
+        stack.push(24);
+        stack.push(6);
+        
+    System.out.println(stack.pop());
+
+    }
+}
