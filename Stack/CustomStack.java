d479f1512dce50357688060f8c2eac9c810a4763
Vikram Kumar <145278357+vikram-2101@users.noreply.github.com>
2024-02-21 22:49:46 +0530

    Stack code

--- Stack/CustomStack.java
+++ Stack/CustomStack.java
@@ -1,49 +1,51 @@
 package Stack;
 
 public class CustomStack {
- protected int[] data;
-private static final int DEFAULT_SIZE = 10;
+    protected int[] data;
+    private static final int DEFAULT_SIZE = 10;
 
- int ptr = -1;
+    int ptr = -1;
 
-public CustomStack(int size) {
-    this.data = new int[size];
-}
-
- public CustomStack() {
-   this(DEFAULT_SIZE);
-}
-
-public boolean push(int item) {
-    if(isFull()) {
-        System.out.println("Stack is full!");
-        return false;
+    public CustomStack(int size) {
+        this.data = new int[size];
     }
-    ptr++;
-    data[ptr] = item;
-    return true;
- }
-   
-  public int pop() throws StackException {
-    if (isEmpty()) {
-        throw new StackException("Cannot pop from empty stack");
-  }
-  int removed = data[ptr];
-  ptr--;
-  return removed;  // simple return data[ptr--];
-}
 
-public boolean isFull() {
-    return ptr == data.length - 1;  // ptr is at last index
-    
-}
-public boolean isEmpty() { 
-    return ptr == -1;
-}
-public int peek() throws StackException {
-    if (isEmpty()) {
-        throw new StackException("Cannot peek from empty stack");
+    public CustomStack() {
+        this(DEFAULT_SIZE);
     }
-    return data[ptr];
-}
+
+    public boolean push(int item) {
+        if (isFull()) {
+            System.out.println("Stack is full!");
+            return false;
+        }
+        ptr++;
+        data[ptr] = item;
+        return true;
+    }
+
+    public int pop() throws StackException {
+        if (isEmpty()) {
+            throw new StackException("Cannot pop from empty stack");
+        }
+        int removed = data[ptr];
+        ptr--;
+        return removed; // simple return data[ptr--];
+    }
+
+    public boolean isFull() {
+        return ptr == data.length - 1; // ptr is at last index
+
+    }
+
+    public boolean isEmpty() {
+        return ptr == -1;
+    }
+
+    public int peek() throws StackException {
+        if (isEmpty()) {
+            throw new StackException("Cannot peek from empty stack");
+        }
+        return data[ptr];
+    }
 }
