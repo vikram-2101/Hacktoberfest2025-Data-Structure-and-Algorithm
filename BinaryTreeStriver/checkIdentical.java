f263af2c95a4823d5b6e17919843c1e770a0fbcb
Vikram Kumar <145278357+vikram-2101@users.noreply.github.com>
2024-03-20 23:24:21 +0530

    Update checkIdentical.java

--- BinaryTreeStriver/checkIdentical.java
+++ BinaryTreeStriver/checkIdentical.java
@@ -1,4 +1,4 @@
-uhjfpublic class checkIdentical {
+public class checkIdentical {
     // The 'isIdentical' function checks if two given binary trees are identical or
     // not.
     // Two binary trees are considered identical if their node values are the same
