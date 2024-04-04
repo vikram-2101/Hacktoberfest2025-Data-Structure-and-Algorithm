f3c38b484ffd61e01734f829e303c3e661e4bdd3
Vikram Kumar <145278357+vikram-2101@users.noreply.github.com>
2024-04-04 20:45:16 +0530

    Update LinearSearch.java

--- Linear Search/LinearSearch.java
+++ Linear Search/LinearSearch.java
@@ -3,7 +3,7 @@ import java.util.Arr
 public class LinearSearch {
     public static void main(String[] args) {
 
-    4    // find the target
+      // find the target
 
         // int[] nums = {23, 45, 1,2 ,8 ,19 ,-6 ,-11, 28};
         // int target =3;
