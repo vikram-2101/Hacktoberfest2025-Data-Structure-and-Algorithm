97939c09132ac688836f7ce6db5c9cea97927fca
Vikram Kumar <145278357+vikram-2101@users.noreply.github.com>
2024-04-11 22:46:10 +0530

    Update DistributeChocolate.java

--- BinarySearch/DistributeChocolate.java
+++ BinarySearch/DistributeChocolate.java
@@ -1,4 +1,4 @@
-public class DistributeChocolate {
+spublic class DistributeChocolate {
     public static void main(String[] args) {
         int[] arr = { 5, 3, 1, 4, 2 };
         int m = 3; // number of students in which to distribute
